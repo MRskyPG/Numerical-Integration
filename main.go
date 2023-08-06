4be61313e68247ea704b6422a229e8860e4a8616
MRskyPG <mrogalsky8113@gmail.com>
2023-08-06 12:16:51 +0600

    Update main.go

--- main.go
+++ main.go
@@ -39,27 +39,27 @@ func main() {
 	Ih := make([]float64, 3)
 
 	for i := 0; i < 3; i++ {
-		//Гаусс
 		//I с текущим шагом i
 		fmt.Printf(`Метод "%s" вычисления определенного интеграла`, Type)
 		I[i] = quadratureFormula1.CalculateIntegral(&begin, &end, numOfSegments*int(math.Pow(2.0, float64(i))), f)
 		Ih[i] = quadratureFormula1.CalculateIntegral(&begin, &end, numOfSegments*int(math.Pow(2.0, float64(i)))*2, f)
 
-		////Трапеции
-		//fmt.Println("Метод Трапеций вычисления определенного интеграла")
-		//I[i] = quadratureFormula2.CalculateIntegral(&begin, &end, numOfSegments*int(math.Pow(2.0, float64(i))), f)
-		//Ih[i] = quadratureFormula2.CalculateIntegral(&begin, &end, numOfSegments*int(math.Pow(2.0, float64(i)))*2, f)
-
 		fmt.Printf("\n%d -------------------------------------------------\n", i+1)
 		fmt.Printf("Для %d отрезков. Шаг h = %g\n", i+1, (end.X-begin.X)/(float64(numOfSegments)*math.Pow(2.0, float64(i))))
 		fmt.Printf("Значение по методу квадратур I = %g\n", I[i])
 		fmt.Printf("Абсолютная погрешность |I_true - I| = %g\n", math.Abs(trueIntegral-I[i]))
+
 		k := math.Round(math.Log2(math.Abs((1 + (Ih[i]-I[i])/(trueIntegral-Ih[i])))))
+
 		fmt.Printf("Порядок аппроксимации k = %d\n\n", int(k))
 		fmt.Printf("Отношение (I_True - I)/(I_True - Ih) = %g\n", (trueIntegral-I[i])/(trueIntegral-Ih[i]))
+
 		R := (Ih[i] - I[i]) / (math.Pow(2.0, k) - 1)
+
 		fmt.Printf("Правило Рунге (Ih - I)/(2^k - 1) = %g\n", R)
+
 		IR := Ih[i] + R
+
 		fmt.Printf("Уточнение по Ричардсону IR = %g\n", IR)
 		fmt.Printf("Погрешность |I_True - IR| = %g\n\n", trueIntegral-IR)
 	}
